412980fd18561afce63eb7df6735bb45c5e2b488
de-scientist <gitaumark502@gmail.com>
2025-11-28 18:16:39 +0300

    update the contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,7 +4,7 @@ const Contact: React.FC = () => {
   return (
     <section className="py-5">
       <div className="container">
-        {/* contact.html is currently empty in your repo */}
+       
         <h1 className="section-title text-center">Contact</h1>
       </div>
     </section>
