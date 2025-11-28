71c2804f860a21300cf41289ff7c3bcdac6cfd29
de-scientist <gitaumark502@gmail.com>
2025-11-28 16:28:56 +0300

    export the resources function component

---
+++ src/pages/Resources.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Resources: React.FC = () => {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <h1 className="section-title text-center">Resources</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Resources;
