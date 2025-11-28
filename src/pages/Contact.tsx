f6151fffd38844e099013cc48e750b8c1eb912c4
de-scientist <gitaumark502@gmail.com>
2025-11-28 16:30:43 +0300

    export the contact function component

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact: React.FC = () => {
+  return (
+    <section className="py-5">
+      <div className="container">
+        {/* contact.html is currently empty in your repo */}
+        <h1 className="section-title text-center">Contact</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
