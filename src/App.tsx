60705f809c02bbe8dc955707acf087d9e228992e
de-scientist <gitaumark502@gmail.com>
2025-11-28 16:35:59 +0300

    update app imports

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,22 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Ministries from "./pages/Ministries";
 import Events from "./pages/Events";
-import Resources from "./pages/Resources";
+import Resources from "./pages/Resources";";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
+// src/App.tsx (add at top with other imports)
+import Chairman from "./pages/committees/Chairman";
+import ViceChair1 from "./pages/committees/ViceChair1";
+import ViceChair2 from "./pages/committees/ViceChair2";
+import Secretary from "./pages/committees/Secretary";
+import ViceSecretary from "./pages/committees/ViceSecretary";
+import Treasurer from "./pages/committees/Treasurer";
+import BibleStudyCommittee from "./pages/committees/BibleStudyCommittee";
+import PrayerCommittee from "./pages/committees/PrayerCommittee";
+import MissionsCommittee from "./pages/committees/MissionsCommittee";
+import MusicCommittee from "./pages/committees/MusicCommittee";
+import TechnicalCommittee from "./pages/committees/TechnicalCommittee";
+import CreativeCommittee from "./pages/committees/CreativeCommittee";
 
 const App: React.FC = () => {
   return (
