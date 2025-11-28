03a63022efe16ec823d2ff8606d1817587b294b7
de-scientist <gitaumark502@gmail.com>
2025-11-28 16:29:51 +0300

    export the gallery function component

---
+++ src/pages/Gallery.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Gallery: React.FC = () => {
+  return (
+    <section className="py-5">
+      <div className="container">
+        <h1 className="section-title text-center">Gallery</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
