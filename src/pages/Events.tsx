e033d76fae2d3a3d9f48e62b453f90fdfdde763e
de-scientist <gitaumark502@gmail.com>
2025-11-28 16:28:01 +0300

    import the event function component

---
+++ src/pages/Events.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Events: React.FC = () => {
+  return (
+    <section className="py-5">
+      <div className="container">
+        {/* events.html is currently empty; fill this as you design the page */}
+        <h1 className="section-title text-center">Events</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Events;
